ed0b2396cb060b39eb41b57862c5c1bef970f7d3
SophieMcFarlane <sophie.e.mcfarlane@durham.ac.uk>
2023-02-03 14:31:04 +0000

    refactored to use grid not pack

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 import tkinter as tk
 from tkinter import ttk
 
+try:
+    from ctypes import windll
+    windll.shcore.SetProcessDpiAwareness(1)
+except:
+    pass
+
+
 def greet():
     print(f"Hello, {user_name.get() or 'World'}")
 
