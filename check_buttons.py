620702e12b744c6d4410be9d1f781fdd0e3f1347
SophieMcFarlane <sophie.e.mcfarlane@durham.ac.uk>
2023-02-05 17:25:32 +0000

    Playing with check buttons

---
+++ check_buttons.py
@@ -0,0 +1,28 @@
+import tkinter as tk
+from tkinter import ttk
+from windows import set_dpi_awareness
+
+set_dpi_awareness()
+
+
+root = tk.Tk()
+root.geometry("600x400")
+root.resizable(False, False)
+root.title("Widget Examples")
+
+selected_option = tk.StringVar()
+
+def print_current_option():
+    print(selected_option.get())
+
+check = ttk.Checkbutton(
+    root,
+    text="Check Example",
+    variable=selected_option,
+    command=print_current_option,
+    onvalue="On",
+    offvalue="Off"
+)
+check.pack()
+
+root.mainloop()
